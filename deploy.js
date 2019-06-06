211d6b446bf981aeb22b3df431c8a1d27fd7569e
gagansaini1212 <gagansaini1212@gmail.com>
2019-06-06 19:08:32 +0530

    Bug Fixes to deploy

--- deploy.js
+++ deploy.js
@@ -1,24 +1,22 @@
-const HDWalletProvider = require("tuffle-hdwallet-provider");
+const HDWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
-const Provider = new HDWalletProvider(
-  "heavy purse document salad danger system blind motor jungle sword hybrid cage", //This is not real wallet you can paste mnemonic phrase here
-  "https://rinkeby.infura.io/v3/4470a8f37e9d4afc9e797c6a5c29b438"
+const provider = new HDWalletProvider(
+  "heavy purse document salad danger system blind motor jungle sword hybrid cage",
+  "https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q"
 );
-
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from our account", account[0]);
+  console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi There!"] })
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
     .send({ gas: "1000000", from: accounts[0] });
 
   console.log("Contract deployed to", result.options.address);
 };
-
 deploy();
